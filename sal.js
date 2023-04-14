d9e32f8c0f22b20e7bd4047d8a5b84dc107ee9f2
ajimenez034 <109635603+ajimenez034@users.noreply.github.com>
2023-04-13 22:52:27 -0600

    Delete sal.js

--- sal.js
+++
@@ -1,9 +0,0 @@
-
-var string = document.getElementById("number");
-var boton = document.getElementById("btn");
-
-boton.addEventListener("click", function() {
-   
-    var numeroActual = parseInt(string.textContent);
-    string.textContent = (numeroActual + 1).toString();
-});
